d7a61d209833e820097a7d8cd609cff6a9309972
arshad <arshad@thedreamsteps.com>
2018-10-22 20:05:09 +0530

    Update change

--- protractor.conf.js
+++ protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'http://18.191.133.127',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
