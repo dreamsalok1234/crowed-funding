38f67619f04620e1fa925ac97859c2d86739b8a7
dreamsalok1234 <dsmail.alok@gmail.com>
2018-10-26 12:17:16 +0530

    works compo

--- src/app/root/how-it-works/works.component.ts
+++ src/app/root/how-it-works/works.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-works',
   templateUrl: './works.component.html',
   styleUrls: ['./works.component.css']
 })
 export class WorksComponent implements OnInit {
-  constructor() { }
+  constructor(public router: Router) { }
   viewMode = 'tab1'
   ngOnInit() {
      
